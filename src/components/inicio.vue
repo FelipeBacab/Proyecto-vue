fa8dce1eb7d32d28be40dfa27f6a08be2398f104
FelipeBacab <felipe.19070004@itsmotul.edu.mx>
2023-02-23 10:00:03 -0600

    proyecto vue

---
+++ src/components/inicio.vue
@@ -0,0 +1,44 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Laboratorio de sistemas y aplicaciones web
+      <br />
+      <br />
+      <img src="http://isclab.com.mx/img/svg/email.svg" alt="email" class="w-6 h-6" width="30" height="20" />Email: contacto@isclab.com.mx
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 20px;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: center;
+  }
+}
+</style>
